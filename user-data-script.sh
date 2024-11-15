5a3f60d1591c326d5bf593367bc02ab315d5541f
jasenmichael <jasen@jasenmichael.com>
2024-11-15 13:45:25 -0600

    update template

--- user-data-script.sh
+++ user-data-script.sh
@@ -4,23 +4,21 @@ echo "APP_USER $APP_USER"
 echo "GH_TO_DROPLET_PUBLIC_KEY $GH_TO_DROPLET_PUBLIC_KEY"
 ls -lan .
 
-exit 1
+apt-get update -y
+apt install ansible -y
 
-# apt-get update -y
-# apt install ansible -y
+# Create APP_USER user
+useradd -m -U -s /bin/bash ${APP_USER}
+usermod -aG sudo ${APP_USER}
 
-# # Create APP_USER user
-# useradd -m -U -s /bin/bash ${APP_USER}
-# usermod -aG sudo ${APP_USER}
+# Set up SSH for APP_USER user
+mkdir -p /home/${APP_USER}/.ssh
+echo "${GH_TO_DROPLET_PUBLIC_KEY}" >/home/${APP_USER}/.ssh/authorized_keys
+chmod 700 /home/${APP_USER}/.ssh
+chmod 600 /home/${APP_USER}/.ssh/authorized_keys
+chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}/.ssh
 
-# # Set up SSH for APP_USER user
-# mkdir -p /home/${APP_USER}/.ssh
-# echo "${GH_TO_DROPLET_PUBLIC_KEY}" >/home/${APP_USER}/.ssh/authorized_keys
-# chmod 700 /home/${APP_USER}/.ssh
-# chmod 600 /home/${APP_USER}/.ssh/authorized_keys
-# chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}/.ssh
-
-# # Grant passwordless sudo access
-# echo "%sudo ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
-# echo "root ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
-# echo "${APP_USER} ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
+# Grant passwordless sudo access
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
+echo "root ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
+echo "${APP_USER} ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
