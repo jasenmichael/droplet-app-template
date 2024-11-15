446e7003c24ac9e215a3a98041ebf4aa99fa34b2
jasenmichael <jasen@jasenmichael.com>
2024-11-15 12:32:31 -0600

    update template

---
+++ user-data-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apt-get update -y
+apt install ansible -y
+
+# Create APP_USER user
+useradd -m -U -s /bin/bash ${APP_USER}
+usermod -aG sudo ${APP_USER}
+
+# Set up SSH for APP_USER user
+mkdir -p /home/${APP_USER}/.ssh
+echo "${GH_TO_DROPLET_PUBLIC_KEY}" >/home/${APP_USER}/.ssh/authorized_keys
+chmod 700 /home/${APP_USER}/.ssh
+chmod 600 /home/${APP_USER}/.ssh/authorized_keys
+chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}/.ssh
+
+# Grant passwordless sudo access
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
+echo "root ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
+echo "${APP_USER} ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
