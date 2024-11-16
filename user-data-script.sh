356211e8c745dfe4e88335bc421ce05b27325155
jasenmichael <jasen@jasenmichael.com>
2024-11-16 12:30:08 -0600

    update template

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-# Create APP_USER user
+# Log files
+COMMAND_LOG_FILE="/var/log/provision-droplet-command-log"
+INIT_LOG_FILE="/var/log/provision-droplet-init-log"
+COMMAND_COUNT=0
+
+# Function to log the last command
+log_command() {
+  ((COMMAND_COUNT++))
+  local cmd_status=$?
+
+  # Log the command and status
+  echo "Command ${COMMAND_COUNT}: $BASH_COMMAND" | tee -a "$COMMAND_LOG_FILE"
+  echo "----------------------------------------------------" | tee -a "$COMMAND_LOG_FILE"
+
+  if [ $cmd_status -ne 0 ]; then
+    echo "Command ${COMMAND_COUNT} failed: $BASH_COMMAND" | tee -a "$COMMAND_LOG_FILE"
+    echo "failed" >"$INIT_LOG_FILE"
+    exit $cmd_status
+  else
+    echo "Command ${COMMAND_COUNT} succeeded: $BASH_COMMAND" | tee -a "$COMMAND_LOG_FILE"
+  fi
+}
+
+# Trap to run log_command after each command
+trap log_command DEBUG
+
+# Start the provisioning script
+
+# Create the APP_USER user and setup environment
 useradd -m -U -s /bin/bash ${APP_USER}
 usermod -aG sudo ${APP_USER}
 
@@ -16,5 +44,9 @@ echo "%sudo ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
 echo "root ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
 echo "${APP_USER} ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
 
+# Update and install Ansible
 apt-get update -y
 apt install ansible -y
+
+# If the script reaches this point, mark success
+echo "succeed" >"$INIT_LOG_FILE"
